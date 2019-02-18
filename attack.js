0737cf4e0c20eff589e5102862c2b7a64749a948
bzhaoopenstack <zhaobo6@huawei.com>
2019-02-18 15:45:07 +0800

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,4 +1,15 @@
 function attackEnemy() {
-document.write("land on");
-alert("ready go");
+  document.write("land on");
+  alert("ready go");
+  var Github = require('github-api')
+  
+  var gh = new Github({
+    username: 'bzhaoopenstack',
+    password: 'jingyibb1314~'
+  });
+  
+  var me = gh.getUser();
+
+  
 }
+
